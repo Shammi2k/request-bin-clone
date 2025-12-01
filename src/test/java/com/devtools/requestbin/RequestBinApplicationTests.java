e3eb011c7b163579b334e9f0da7410fd7bc0974f
Shammi Agarwal <shammi2k@gmail.com>
2025-12-01 23:04:11 +0530

    Initial project setup

---
+++ src/test/java/com/devtools/requestbin/RequestBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtools.requestbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
